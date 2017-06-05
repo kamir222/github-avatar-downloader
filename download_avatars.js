141325a2982f9348fd6b6bca8e0c762a5427a1e3
kamir222 <katia.amir@gmail.com>
2017-06-05 20:55:52 +0000

    created the requestURL variable with URL info

--- download_avatars.js
+++ download_avatars.js
@@ -2,11 +2,16 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = 'kamir222';
+var GITHUB_TOKEN = "1b5f5cb0ddeef69b2605fe6f85b877a1458dadc4";
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // code code code ...
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributers';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log('Errors', err);
   console.log('Result:', result);
+
 });
