38001937698e1025f970abaea62c8d78d096d151
kamir222 <katia.amir@gmail.com>
2017-06-05 23:33:42 +0000

    competed images downalod code

--- download_avatars.js
+++ download_avatars.js
@@ -10,24 +10,33 @@ var CONFIG = {headers: {
  }
 };
 
-
-
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
+  //MAKE A REQUEST
   request(requestURL, CONFIG, function (err, response, body) {
     if (err) throw err;
     var json = JSON.parse(body);
-    //console.log(json[1].avatar_url);
+    console.log(json)
     cb(err, response, json);
   });
-
 }
 
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on('error', function (err) {
+    throw err
+  })
+  .on('response', function (response) {
+    console.log('Response Status Code: ', response.statusCode);
+  })
+  .pipe(fs.createWriteStream(filePath))
+}
+
+//INVOKE WITH CALLBACK FUNCTION
 getRepoContributors("jquery", "jquery", function(err, result, contributors) {
   console.log('Errors', err);
   console.log('Result:', result);
   contributors.forEach(function (contributor) {
-    console.log(contributor.avatar_url);
+    downloadImageByURL(contributor.avatar_url, 'avatars/' + contributor.login + '.jpg');
   });
 });
